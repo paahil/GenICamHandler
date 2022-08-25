a42470d0392d8b06f9969023c303c07318156e5a
Paavo Hiltunen <paavo.hiltunen@aalto.fi>
2022-08-25 14:27:35 +0300

    Added Comments

--- src/camHandler.py
+++ src/camHandler.py
@@ -8,44 +8,55 @@ import datetime
 import numpy
 import os
 
+# Static Defines
 colorforms = ["BayerRG8"]
 pcktsizes = [1440, 2960, 4480, 6000, 7520, 9040, 10560]
 
+# Class for interfacing with the physical device
+
 class CamHandler:
+    # Class initialization
     def __init__(self):
-        self.errlog = None
-        self.logfname = None
-        self.cam = None
-        self.camprops = None
-        self.savepth = None
-        self.thrsh = 0
-        self.bufnum = 6
-        self.partw = 256
-        self.parth = 256
-        self.offsetx = 900
-        self.offsety = 900
-        self.fpslimit = 1
-        self.defH = 0
-        self.defW = 0
-        self.defOffX = 0
-        self.defOffY = 0
+        # Variables for storing runtime variables
+        self.errlog = None  # Variable for current error log
+        self.logfname = None  # Variable for the filename of the current error log
+        self.cam = None  # Variable for current physical device
+        self.camprops = None  # Variable shorthand to access properties of the current device
+        self.savepth = None  # Variable for current save directory path
+        self.thrsh = 0  # Variable for binarization threshold
+        self.bufnum = 6  # Variable for used number of buffers
+        self.partw = 256  # Variable for partial scan width (in pixels)
+        self.parth = 256  # Variable for partial scan height (in pixels)
+        self.offsetx = 900  # Variable for partial scan horizontal offset (in pixels)
+        self.offsety = 900  # Variable for partial scan vertical offset (in pixels)
+        self.fpslimit = 1  # Variable for FPS limit
+        self.defH = 0  # Variable for saving the original height (for toggling partial scan off)
+        self.defW = 0  # Variable for saving the original width (for toggling partial scan off)
+        self.defOffX = 0  # Variable for saving the original horizontal offset (for toggling partial scan off)
+        self.defOffY = 0  # Variable for saving the original vertical offset (for toggling partial scan off)
 
-        self.limit = False
-        self.partial = False
-        self.triggering = False
-        self.acquire = False
-        self.filtering = False
-        self.color = False
-        self.saving = False
-        self.systime0 = None
-        self.tstamp0 = 0
-        self.sync = False
+        # Boolean variables for toggle switches
+        self.limit = False  # Is the FPS limiter enabled?
+        self.partial = False  # Is partial scanning enabled?
+        self.triggering = False  # Is triggered acquisition enabled?
+        self.acquire = False  # Is image acquisition enabled?
+        self.filtering = False  # Is image filtering (thresholding) enabled?
+        self.color = False  # Is the device using color format (BAYERNRG8)
+        self.saving = False  # Is image storing enabled?
 
-        self.harvester = Harvester()
+        # Variables for timestamp usage
+        self.systime0 = None  # Variable for system timestamp acquired with the first frame
+        self.tstamp0 = 0  # Variable for device timestamp acquired with the first frame
+        self.sync = False  # Are variables systime0 and tstamp0 synchronized i.e. frame count > 1
+
+        self.harvester = Harvester()  # Initialize the harvester class
+        #  Add a cti file to the harvester
+        #  MODIFY THIS TO MATCH YOUR MATRIX VISION INSTALLATION PATH!!!
         self.harvester.add_file("C:\\Users\\Paavo\\Documents\\ADENN2021\\MATRIX VISION\\bin\\x64\\mvGenTLProducer.cti")
         #self.load()
-        self.openerrlog()
+        self.openerrlog()  # Open the error log for runtime logging
 
+    # Method for loading saved configurations (Currently not used)
     def load(self):
         try:
             file = open('cfgs/default.cfgh', 'r')
@@ -62,6 +73,7 @@ class CamHandler:
         except FileNotFoundError:
             pass
 
+    # Method for saving current configurations for later use
     def save(self):
         file = open('cfgs/default.cfgh', 'w')
         file.write('%s\n' % self.savepth)
@@ -73,6 +85,7 @@ class CamHandler:
         file.write('%d\n' % self.offsety)
         file.close()
 
+    # Method for running the parameter load function on the end device
     def loadCameraProperties(self):
         if self.camprops is not None:
             try:
@@ -86,6 +99,7 @@ class CamHandler:
                 except genicam.genapi.LogicalErrorException:
                     pass
 
+    # Method for running the parameter save function on the end device
     def saveCameraProperties(self):
         if self.camprops is not None:
             try:
@@ -99,6 +113,7 @@ class CamHandler:
                 except genicam.genapi.LogicalErrorException:
                     pass
 
+    # Method for initializing properties on the end device to comply with the software
     def initCamera(self):
         if self.camprops is not None:
             try:
@@ -115,6 +130,7 @@ class CamHandler:
                 except genicam.genapi.LogicalErrorException:
                     pass
 
+    # Method for opening the error log
     def openerrlog(self):
         if not os.path.exists(os.path.join(os.getcwd(), 'logs')):
             os.mkdir('logs')
@@ -129,19 +145,24 @@ class CamHandler:
         if os.path.getsize(self.logfname) == 0:
             os.remove(self.logfname)
 
+    # Method for logging errors with the specified message
+    # Input: message, string containing the desired error message
     def logerror(self, message):
         tstamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
         self.errlog.write("{0} ERROR: ".format(tstamp) + message + '\n')
 
+    # Method for changing active device
+    # Input: ind, index of the desired device in harvester.device_info_list
     def changeCam(self, ind):
-        if self.cam is not None:
-            self.acquire = False
+        if self.cam is not None:  # If another device is in use disconnect it before activating new device
+            self.acquire = False  # Verify that acquisition is stopped before disconnecting
             while self.cam.is_acquiring():
                 time.sleep(0.01)
-            self.saveCameraProperties()
-            self.cam.destroy()
-            self.cam = None
-            self.camprops = None
+
+            self.saveCameraProperties()  # Save configured device properties to device memory
+            self.cam.destroy()  # Destroy the link to the old device
+            self.cam = None  # Set the current device variable to None to indicate no device is active
+            self.camprops = None  # Reset device property helper variable
         if 0 <= ind < len(self.harvester.device_info_list):
             try:
                 self.cam = self.harvester.create_image_acquirer(ind)
@@ -169,6 +190,9 @@ class CamHandler:
                 self.cam = None
                 self.camprops = None
 
+    # Method for acquiring a single frame
+    # return: arr, numpy array containing the pixel values of the acquired image
+    # return: syststamp, system timestamp for the acquired image (calculated from the timestamp given by the device)
     def acquireImag(self):
         if self.cam.is_acquiring():
             try:
@@ -188,6 +212,9 @@ class CamHandler:
                 pass
         return None, 0
 
+    # Method for getting the value of the desired property
+    # input: prop, string containing the name of the property
+    # return: value of the property
     def getProperty(self, prop):
         ret = None
         if self.camprops is not None:
@@ -332,6 +359,9 @@ class CamHandler:
                         pass
         return ret
 
+    # Method for setting the value of the desired device property
+    # input: prop, String containing the name of the property
+    # input: val, Desired value for the property. Type depends on the property
     def setProperty(self, prop, val=None):
         if self.camprops is not None:
             if prop == "Width":
@@ -388,6 +418,7 @@ class CamHandler:
                     except genicam.genapi.LogicalErrorException:
                         pass
 
+    # Method for toggling the FPS limiter
     def toggleFPSLimit(self):
         if self.cam is not None:
             if self.limit:
@@ -397,6 +428,7 @@ class CamHandler:
                 self.setProperty("FPS", self.fpslimit)
                 self.limit = True
 
+    # Method for toggling partial scanning
     def togglePartial(self):
         if self.partial:
             self.setProperty("OffsetX", self.defOffX)
@@ -411,6 +443,7 @@ class CamHandler:
             self.setProperty("OffsetY", self.offsety)
             self.partial = True
 
+    # Method for toggling triggering
     def toggleTrigger(self):
         if self.cam is not None:
             if self.triggering:
@@ -434,24 +467,34 @@ class CamHandler:
                     except genicam.genapi.LogicalErrorException:
                         pass
 
+    # Method to change the number of buffers that can be used
     def changeBufnum(self, val):
         if self.cam is not None:
             self.bufnum = val
             self.cam.num_buffers = self.bufnum
 
+    # Method for filtering the image (threshold)
+    # input: frame, image frame to be filtered
+    # return: frame, filtered image frame (if filtering is toggled off returns the input)
     def filtImag(self, frame):
         if self.filtering:
             frame = cv.threshold(frame, self.thrsh, 255, cv.THRESH_TOZERO)[1]  # Threshold the image
         return frame
 
-    def saveImag(self, bw, timestamp):
+    # Method for saving a image
+    # input: frame, image frame to be saved
+    # input: timestamp, timestamp of the image frame
+    def saveImag(self, frame, timestamp):
         if self.saving:
             if self.savepth is None:
-                cv.imwrite(timestamp + '.jpg', bw)
+                cv.imwrite(timestamp + '.jpg', frame)
             else:
                 fname = "{0}{1}.jpg".format(self.savepth, timestamp)
-                cv.imwrite(fname, bw)
+                cv.imwrite(fname, frame)
 
+    # Method for changing the image storage directory
+    # input: dirname, absolute path to the directory
+    # return: boolean, was the change successful?
     def changeSaveDir(self, dirname):
         if os.path.isdir(dirname):
             self.savepth = dirname + '\\'
@@ -459,11 +502,16 @@ class CamHandler:
         else:
             return False
 
+    # Method for syncing the system timestamp with the device timestamp
+    # input: tstamp0, the device timestamp of the first acquired frame
     def synctimestamp(self, tstamp0):
         self.systime0 = datetime.datetime.now()
         self.tstamp0 = tstamp0
         self.sync = True
 
+    # Method for acquiring a system timestamp based on current synchronization and the device timestamp
+    # input: tstamp, device timestamp used for conversion
+    # return: syststamp, system timestamp
     def getsystimestamp(self, tstamp):
         hour = self.systime0.hour
         min = self.systime0.minute
